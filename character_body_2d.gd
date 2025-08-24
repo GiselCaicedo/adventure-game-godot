74f7bfdad1eaa49d5b955c0ae6f0fe08918101b3
Gisel Caicedo <giselcaicedosoler@gmail.com>
2025-08-24 00:33:59 -0500

    Se rediseña escensario y personaje

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 400.0
-const JUMP_VELOCITY = -700.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
 
 # --- Nuevas variables para el doble salto ---
 const MAX_JUMPS = 2 # Define la cantidad máxima de saltos (1 = normal, 2 = doble, etc.)
